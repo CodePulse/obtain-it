d993ebb20983902b42e8220f4d98722b2734006e
Ben Lamptey <ben@codepulse.co.uk>
2018-08-21 10:38:39 +0100

    remove contrib modules from repo

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -783,14 +783,4 @@ $settings['entity_update_batch_size'] = 50;
  if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
    include $app_root . '/' . $site_path . '/settings.local.php';
  }
-$databases['default']['default'] = array (
-  'database' => 'drupal',
-  'username' => 'root',
-  'password' => 'password',
-  'prefix' => '',
-  'host' => 'obtain-it-new.localhost',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
 
